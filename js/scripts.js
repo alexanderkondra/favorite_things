769ba636eae9b332f54b0fee980f8a61bdfd1772
Alexander Kondra and Jess Doss <alexander@>
2019-06-10 12:02:22 -0700

    display answers from users on webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  alert("test");
   $("form").submit(function(event){
   var color = $("input#color").val();
   var music = $("input#music").val();
@@ -8,5 +7,12 @@ $(document).ready(function(){
   var total = [color, music, place, food];
   console.log(total);
   event.preventDefault();
+  // total.push(total[2]);
+  console.log(total);
+  // This will pull the text from the form and display in a list.
+  $("#colorOutput").text(total[0]);
+  $("#musicOutput").text(total[1]);
+  $("#placeOutput").text(total[2]);
+  $("#foodOutput").text(total[3]);
   })
 });
