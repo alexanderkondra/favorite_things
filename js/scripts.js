ca0d1cc6ecf663faadfe0e277f963b20e8ad21fd
Alexander Kondra and Jess Doss <jess@>
2019-06-10 13:15:03 -0700

    add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
+// getting document ready
+
   $("form").submit(function(event){
+// we grabbing ("form")- in html with .submit class button
+
   var color = $("input#color").val();
   var music = $("input#music").val();
   var place = $("input#place").val();
   var food = $("input#food").val();
+// grab data from html with "input type and id #color , #music" .val make it value (number)
+
   var total = [color, music, place, food];
-  console.log(total);
-  event.preventDefault();
-  // total.push(total[2]);
-  console.log(total);
-  // This will pull the text from the form and display in a list.
+// create var for all of them whitch is total
+
   $("#colorOutput").text(total[0]);
   $("#musicOutput").text(total[1]);
   $("#placeOutput").text(total[2]);
   $("#foodOutput").text(total[3]);
+  // This will pull the text from the form and display in a list.
   })
 });
